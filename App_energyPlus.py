6e17c64fb5db7df98b6510c74d140e5e415b6eb9
goncalo-araujo <81910852+goncalo-araujo@users.noreply.github.com>
2022-03-28 15:00:10 +0100

    Update App_energyPlus.py

--- App_energyPlus.py
+++ App_energyPlus.py
@@ -118,7 +118,7 @@ def user_input_features():
     prop = st.sidebar.slider('Proporção', 1.00, 5.00, 2.00, 0.01)
     N_floors = st.sidebar.slider('Número de pisos', round(X.nfloor.min()), round(X.nfloor.max()), round(X.nfloor.mean()))
     rot = st.sidebar.slider('Orientação radianos em relação a Este', round(X.rot.min(), 2), round(X.rot.max(), 2), 0.00)
-    wwr = st.sidebar.slider('Rácio de envidraçado', round(X.wwr.min(), 2), round(X.wwr.max(), 2), round(X.wwr.mean(), 2))
+    wwr = st.sidebar.slider('Rácio de envidraçado', round(X.wwr.min(), 2), round(X.wwr.max(), 2), 0.3)
     data = {"Número de pisos": N_floors,
             "Proporção rectangular do apartamento": prop,
             "Orientação radianos em relação a Este": rot,
